79024cbf065452d8621f6104a817ae62ab9c4dbe
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-02-09 15:10:50 +0200

    Created login function in Engine class

--- core/engine.py
+++ core/engine.py
@@ -1,3 +1,22 @@
+from core.consts import Resources
+from core.storage import get_data
+from core.models.users import User
+from core.errors.users import UserNameDoesntExistException, IncorrectPasswordException
+
 class Engine:
 	def __init__(self):
-		pass
+		self.user_id: str = None
+		self.user: User = None
+
+	def login(self, user_name: str, password: str):
+		users = get_data(Resources.system_users_path)
+		for id, user in users:
+			if user[Resources.user_name] == user_name:
+				if user[Resources.password] == password:
+					self.user_id = id
+					self.user = user
+				else:
+					raise IncorrectPasswordException
+
+		if not self.user:
+			raise UserNameDoesntExistException
