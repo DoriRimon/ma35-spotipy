490b47adbf34c4bc2ea08d384b998031329fca0c
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-02-09 19:19:59 +0200

    Craeted parse_user_playlists function in parse file

--- core/storage/parse.py
+++ core/storage/parse.py
@@ -1,8 +1,10 @@
-from core.consts import id, song_name, popularity, songs_path, path_delimiter, track
+from core.consts import id_key, song_name_key, popularity_key, songs_path, \
+	user_dir_path, playlists_dir, path_delimiter, track_key, \
+	playlist_name_key, playlist_songs_key
 import json
 import os
-from core.models.music import Song
-from typing import List
+from core.models.music import Song, Playlist
+from typing import List, Dict
 
 
 def parse_songs() -> List[Song]:
@@ -14,7 +16,24 @@ def parse_songs() -> List[Song]:
 	for file_path in songs_paths:
 		with open(file_path) as file:
 			song_json = json.load(file)
-			song_json = song_json[track]
-			songs.append(Song(song_json[id], song_json[song_name], song_json[popularity]))
+			song_json = song_json[track_key]
+			songs.append(Song(song_json[id],
+			                  song_json[song_name_key],
+			                  song_json[popularity_key]))
 
 	return songs
+
+def parse_user_playlists(user_id: str) -> List[Playlist]:
+	playlists_dir_path = user_dir_path(user_id) + path_delimiter + playlists_dir
+	playlists_paths = os.listdir(playlists_dir_path)
+
+	playlists_paths = (list(map(lambda playlist_path: playlists_dir_path + path_delimiter + playlist_path, playlists_paths)))
+	playlists = []
+	for file_path in playlists_paths:
+		with open(file_path) as file:
+			playlist_json = json.load(file)
+			playlists.append(Playlist(playlist_json[id_key],
+			                          playlist_json[playlist_name_key],
+			                          playlist_json[playlist_songs_key]))
+
+	return playlists
