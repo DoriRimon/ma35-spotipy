50611182a9dae0e785e50d00f2f7871a7aeded03
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-02-09 17:57:33 +0200

    Adapted to consts change

--- core/storage/parse.py
+++ core/storage/parse.py
@@ -1,4 +1,4 @@
-from core.consts import Resources
+from core.consts import id, song_name, popularity, songs_path
 import json
 import os
 from core.models.music import Song
@@ -6,12 +6,12 @@ from typing import List
 
 
 def parse_songs() -> List[Song]:
-	songs_dir_path = Resources.songs_path
+	songs_dir_path = songs_path
 	songs_paths = os.listdir(songs_dir_path)
 
 	songs = []
 	for file in songs_paths:
 		song_json = json.load(file)
-		songs.append(Song(song_json[Resources.id], song_json[Resources.song_name], song_json[Resources.popularity]))
+		songs.append(Song(song_json[id], song_json[song_name], song_json[popularity]))
 
 	return songs
