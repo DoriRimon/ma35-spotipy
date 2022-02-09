3a150b79dc8ade9340d62b6f59dce4912d059110
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-02-09 19:19:19 +0200

    Updated write file to support consts names

--- core/storage/write.py
+++ core/storage/write.py
@@ -1,14 +1,15 @@
 from typing import List
-from core.consts import playlist_name, playlist_date, playlist_songs, user_playlist_path
+from core.consts import id_key, playlist_name_key, playlist_date_key, playlist_songs_key, user_playlist_path
 import time
 import json
 
 
 def write_user_playlist(user_id: str, playlist_id: str, playlist_name: str,
                         songs: List[str]):
-	playlist_dict = {playlist_name: playlist_name,
-	                 playlist_date: time.asctime(),
-	                 playlist_songs: songs}
+	playlist_dict = {id_key: playlist_id,
+	                 playlist_name_key: playlist_name,
+	                 playlist_date_key: time.asctime(),
+	                 playlist_songs_key: songs}
 
 	path = user_playlist_path(user_id, playlist_id)
 	with open(path, 'w') as file:
