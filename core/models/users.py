7b43f130857b90494b527d07360910f1eebf0693
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-02-09 14:01:42 +0200

    Created BasicUser and Artist classes in users file

--- core/models/users.py
+++ core/models/users.py
@@ -1,4 +1,23 @@
+from typing import List
+
+from core.consts import UserType
+from core.models.music import Album
+
+
 class User:
 	def __init__(self, id: str, name: str):
 		self.id = id
 		self.name = name
+
+
+class BasicUser(User):
+	def __int__(self, id: str, name: str, type: UserType):
+		super().__init__(id, name)
+		self.type = type
+
+
+class Artist(User):
+	def __init__(self, id: str, name: str, albums: List[Album]):
+		super().__init__(id, name)
+		self.type = UserType.ARTIST
+		self.albums = albums
