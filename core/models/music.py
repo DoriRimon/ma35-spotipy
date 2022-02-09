a5839f5b96b3184b6542b12c18bc06ca9557fd95
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-02-09 11:17:29 +0200

    Created MusicObject class in music file

--- core/models/music.py
+++ core/models/music.py
@@ -1,5 +1,11 @@
-class Song:
-	def __init__(self, id: str, name: str, popularity: int):
+from typing import List
+
+class MusicObject:
+	def __init__(self, id: str, name: str):
 		self.id = id
 		self.name = name
+
+class Song(MusicObject):
+	def __init__(self, id: str, name: str, popularity: int):
+		super().__init__(id, name)
 		self.popularity = popularity
