47790328ad1b72a1cb2f4bc9822b91e64362fe79
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-02-10 14:06:52 +0200

    Created logs support in write file

--- core/storage/write.py
+++ core/storage/write.py
@@ -1,7 +1,10 @@
 from typing import List
-from core.consts import id_key, name_key, playlist_date_key, playlist_songs_key, user_playlist_path
+from core.consts import id_key, name_key, playlist_date_key, playlist_songs_key, user_playlist_path, Logs
 import time
 import json
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 def write_user_playlist(user_id: str, playlist_id: str, playlist_name: str,
@@ -14,3 +17,5 @@ def write_user_playlist(user_id: str, playlist_id: str, playlist_name: str,
 	path = user_playlist_path(user_id, playlist_id)
 	with open(path, 'w') as file:
 		json.dump(playlist_dict, file)
+
+	logger.debug(Logs.write_successfully("playlist"))
