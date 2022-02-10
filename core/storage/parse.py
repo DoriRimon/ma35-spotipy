18c8fe4c64ab1b91371fe3441e554913649c1149
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-02-10 09:13:35 +0200

    Created parse_all_users function in parse file

--- core/storage/parse.py
+++ core/storage/parse.py
@@ -1,10 +1,14 @@
-from core.consts import id_key, song_name_key, popularity_key, songs_path, \
+from core.consts import UserType, id_key, name_key, popularity_key, songs_path, \
 	user_dir_path, playlists_dir, path_delimiter, track_key, \
-	playlist_name_key, playlist_songs_key
+	name_key, playlist_songs_key, resources_dir, users_dir, metadata_file, json_type, type_key, albums_key
+
 import json
 import os
+
 from core.models.music import Song, Playlist
-from typing import List, Dict
+from core.models.users import BasicUser, Artist
+
+from typing import List
 
 # TODO - separate duplicate code in parse_songs and parse_user_playlists
 
@@ -18,8 +22,8 @@ def parse_songs() -> List[Song]:
 		with open(file_path) as file:
 			song_json = json.load(file)
 			song_json = song_json[track_key]
-			songs.append(Song(song_json[id],
-			                  song_json[song_name_key],
+			songs.append(Song(song_json[id_key],
+			                  song_json[name_key],
 			                  song_json[popularity_key]))
 
 	return songs
@@ -34,7 +38,34 @@ def parse_user_playlists(user_id: str) -> List[Playlist]:
 		with open(file_path) as file:
 			playlist_json = json.load(file)
 			playlists.append(Playlist(playlist_json[id_key],
-			                          playlist_json[playlist_name_key],
+			                          playlist_json[name_key],
 			                          playlist_json[playlist_songs_key]))
 
 	return playlists
+
+def parse_all_users():
+	users_dir_path = path_delimiter.join([resources_dir, users_dir])
+	users_metadata_paths = [path_delimiter.join([
+		users_dir_path,
+		inner,
+		metadata_file]) + "." + json_type for inner in os.listdir(users_dir_path)]
+
+	users = []
+	for file_path in users_metadata_paths:
+		with open(file_path) as file:
+			user_json = json.load(file)
+			user_type = user_json[type_key]
+			if user_type == UserType.PREMIUM.value or user_type == UserType.BASIC.value:
+				users.append(BasicUser(
+					user_json[id_key],
+					user_json[name_key],
+					user_type
+				))
+			else:
+				users.append(Artist(
+					user_json[id_key],
+					user_json[name_key],
+					user_json[albums_key]
+				))
+
+	return users
