87be481caf81f11a7d4c1069653a77a824a305cc
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2022-02-10 16:28:22 +0200

    Created get_artists function in SpotipyRest class

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from console import SpotipyConsole
+from rest import SpotipyRest
 
 def main():
-	sc = SpotipyConsole()
-	sc.show()
+	# sc = SpotipyConsole()
+	# sc.show()
+
+	sr = SpotipyRest()
+	sr.run()
 
 
 if __name__ == "__main__":
--- rest.py
+++ rest.py
@@ -3,7 +3,7 @@ from core.engine import Engine
 from flask import Flask, request
 app = Flask(__name__)
 
-class SpotipyRest:
+class SpotipyRest:  # in work - class is not finished
 	def __init__(self):
 		self.engine = Engine()
 
@@ -12,3 +12,12 @@ class SpotipyRest:
 		user_name = request.args.get("username")
 		password = request.args.get("password")
 		self.engine.login(user_name, password)
+
+	@app.route("/artists", methods=["GET"])
+	def get_artists(self):
+		artists = self.engine.get_all_artists()
+		return artists
+
+	@staticmethod
+	def run():
+		app.run()
